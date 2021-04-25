55a93d70cc039df8493d6ef17fa1f511201e199c
lordlodis <ngocna@gmail.com>
2021-04-25 14:21:33 +0200

    Added rating to list

--- src/main/java/com/alphaone/example/ui/book/model/Book.java
+++ src/main/java/com/alphaone/example/ui/book/model/Book.java
@@ -1,21 +1,26 @@
 package com.alphaone.example.ui.book.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Book {
-	
+
 	private long id;
 
 	private String title;
 
 	private String author;
 
+	private BookRating avgRating;
+
 	protected Book() {
 	}
-	
-	public Book(long id, String title, String author) {
+
+	public Book(long id, String title, String author, BookRating avgRating) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.author = author;
+		this.avgRating = avgRating;
 	}
 
 	public long getId() {
@@ -42,4 +47,19 @@ public class Book {
 		this.author = author;
 	}
 
+	public BookRating getAvgRating() {
+		return avgRating;
+	}
+
+	public void setAvgRating(BookRating avgRating) {
+		this.avgRating = avgRating;
+	}
+	
+	public double getRating() {
+		if (avgRating == null) {
+			return 0;
+		} else {
+			return avgRating.getAvgRating();
+		}
+	}
 }
---
+++ src/main/java/com/alphaone/example/ui/book/model/BookRating.java
@@ -0,0 +1,24 @@
+package com.alphaone.example.ui.book.model;
+
+public class BookRating {
+
+	private double avgRating;
+
+	public BookRating() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public BookRating(double avgRating) {
+		super();
+		this.avgRating = avgRating;
+	}
+
+	public double getAvgRating() {
+		return avgRating;
+	}
+
+	public void setAvgRating(double avgRating) {
+		this.avgRating = avgRating;
+	}
+
+}
