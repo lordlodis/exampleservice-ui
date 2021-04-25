4f3515b1cdf9b36970178498a9a00603c7f71d00
lordlodis <ngocna@gmail.com>
2021-04-25 13:44:45 +0200

    Allowed local connection in dev mode, and real eureka connection in prod mode

--- src/main/java/com/alphaone/example/ui/book/serviceclient/BookServiceClient.java
+++ src/main/java/com/alphaone/example/ui/book/serviceclient/BookServiceClient.java
@@ -14,7 +14,7 @@ import com.alphaone.example.ui.book.model.Book;
  * @author Ngoc
  *
  */
-@FeignClient(value = "book-service")
+@FeignClient(value = "book-service", url = "${feign.book.url}")
 public interface BookServiceClient {
 
 	/**
